e09ff04e8a35bbc0f8a3fd564e89d90a5e6fa60b
DrArtemi <adrien.milcent@outlook.com>
2021-02-15 11:16:57 +0100

    change submit to button

--- ocr-cezam/src/components/AddFiles.js
+++ ocr-cezam/src/components/AddFiles.js
@@ -1,31 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 
 class AddFiles extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            files: [
-                "Pièce d'identité",
-                "Relevé banquaire",
-                "Avis d'imposition",
-                "Tableaux d'amortissement",
-                "Liasse fiscale"
-            ]
+            files: {
+                pieceIdentite: {
+                    name: "Pièce d'identité",
+                    file: null
+                },
+                releveBanquaire: {
+                    name: "Relevé banquaire",
+                    file: null
+                },
+                avisImposition: {
+                    name: "Avis d'imposition",
+                    file: null
+                },
+                tableauAmortissement: {
+                    name: "Tableau d'amortissement",
+                    file: null
+                },
+                liasseFiscale: {
+                    name: "Liasse fiscale",
+                    file: null
+                }
+            }
         }
     }
 
     onChangeHandler = (event) => {
-        console.log('Changed !');
+        // console.log(event.target.files[0]);
+        const newDict = { ...this.state.files[event.target.name], file: event.target.files[0] };
+        const newFiles = { ...this.state.files, [event.target.name]: newDict };
+        this.setState({
+            files: newFiles
+        });
+    }
+
+    onSubmitHandler = () => {
+        const data = new FormData();
+        for (let file in this.state.files) {
+            data.append(file, this.state.files[file]["file"]);
+        }
+
+        // axios
+        // .post("http://localhost:8000/upload", data)
+        // .then((res) => {
+        //     alert("File Upload success");
+        // })
+        // .catch((err) => {
+        //     alert("File Upload Error")
+        // });
     }
 
     render() {
         let inputs = [];
-        for (let file of this.state.files) {
-            console.log(file)
+        for (let file in this.state.files) {
+            console.log(this.state.files[file]["file"]);
             inputs.push(
-                <div className="cz-file-elem">
-                    <label className="cz-label" for={file}>{file}</label>
-                    <input className="cz-input" key={file} type="file" name={file} onChange={this.onChangeHandler}/>
+                <div key={file} className="cz-file-elem">
+                    <label className="cz-label" htmlFor={file}>{this.state.files[file]["name"]}</label>
+                    <input className="cz-input" type="file" name={file} onChange={this.onChangeHandler}/>
                 </div>
             );
         }
@@ -36,7 +73,7 @@ class AddFiles extends React.Component {
                     <h1 className="cz-form-title">Insérez les documents à traiter</h1>
                     {inputs}
                     <div className="cz-form-but">
-                    <input type='submit' value="Lancer l'analyse"/>
+                        <button type="button" onClick={this.onSubmitHandler}>Lancer l'analyse</button>
                     </div>
                 </form>
             </div>
